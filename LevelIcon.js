d6cc9833067d229ee1bdacde2e19dbd7cbbd81b1
andy <andy2472625@gmail.com>
2018-11-09 18:22:37 +0800

    feat:
     - add feedback

--- LevelIcon.js
+++ LevelIcon.js
@@ -5,27 +5,35 @@
  * via webpack's dynamic import.
  */
 
+ /**
+  * Feedback:
+  * We can add .catch after .then to handle error status
+  * Add componentDidUpdate to update while props change
+  * and add isMount to avoid setState on component unmount
+ */
+
 import React from 'react';
 import defaultImage from '../assets/default-image.svg';
 
 class LevelIcon extends React.Component {
   constructor(props) {
     super(props);
-
+    this.isMount = true;
     this.state = {
       src: defaultImage,
     };
   }
 
   componentDidMount() {
-    const { level } = this.props;
+    this._loadImage();
+  }
 
-    import(`../assets/${level}.svg`)
-      .then(mod => {
-        this.setState({
-          src: mod.default
-        })
-      });
+  componentDidUpdate(prevProps) {
+    this._loadImage(prevProps);
+  }
+
+  componentWillUnmount() {
+    this.isMount = false;
   }
 
   render() {
@@ -34,6 +42,22 @@ class LevelIcon extends React.Component {
     return (
       <img src={src} />
     );
+  }
+
+  _loadImage(prevProps) {
+    const { level: preLevel } = prevProps || '';
+    const { level } = this.props || '';
+    
+    if (preLevel === level) return;
+    import(`../assets/${level}.svg`)
+      .then(mod => {
+        this.isMount && this.setState({
+          src: mod.default
+        })
+      })
+      .catch(err => {
+        // do something
+      });
   }
 }
 
--- login.js
+++ login.js
@@ -6,15 +6,27 @@
  * The user can click on logout button to logout and clear the state and storage.
  */
 
+ /**
+  * Feedback:
+  * We should post 'account' & 'password' by headers not params to protect user
+  * and use JSON.stringify while set localStorage.
+ */
+
 const user = {
   userInfo: {},
   login(account, password) {
-    fetch(`https://example.com/api/login?account=${account}&password=${password}`)
+    fetch(`https://example.com/api/login`, {
+      headers: {
+        account,
+        password,
+      },
+      method: 'POST',
+    })
       .then(userInfo => {
         this.userInfo = userInfo;
       });
 
-    window.localStorage.setItem('login', this.userInfo);
+    window.localStorage.setItem('login', JSON.stringify(this.userInfo));
   },
   logout: () => {
     this.userInfo = {};
